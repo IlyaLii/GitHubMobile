6b6287d5ae2298ce80e7b097306a51b1fbfd1d58
Ilya <agronat88@icloud.com>
2020-02-29 12:39:00 +0300

    create project

---
+++ GitHubMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubMobile
+//
+//  Created by Li on 2/29/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
