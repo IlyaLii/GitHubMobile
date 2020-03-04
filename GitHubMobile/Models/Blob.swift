a10c702813613143ed07f8cd50a284369cdca792
Ilya <agronat88@icloud.com>
2020-03-05 00:04:36 +0300

    added imageBase64Download

---
+++ GitHubMobile/Models/Blob.swift
@@ -0,0 +1,13 @@
+//
+//  Blob.swift
+//  GitHubMobile
+//
+//  Created by Li on 3/4/20.
+//  Copyright © 2020 Li. All rights reserved.
+//
+
+import Foundation
+
+struct Blob: Decodable {
+    var content: String
+}
--- GitHubMobile/Network/NetworkManager.swift
+++ GitHubMobile/Network/NetworkManager.swift
@@ -14,12 +14,25 @@ class NetworkManager {
         guard let url = URL(string: url) else { return }
         let session = URLSession.shared
         
-        session.dataTask(with: url) { (data, _, error) in
+        session.dataTask(with: url) { (data, _, _) in
             guard let data = data else { return }
             guard let image = UIImage(data: data) else { return }
             DispatchQueue.main.async {
                 completionHandler(image)
             }
+        }.resume()
+    }
+    
+    static func downloadBase64Image(url: String, completionHandler: @escaping(UIImage) -> Void) {
+        guard let url = URL(string: url) else { return }
+        let sesion = URLSession.shared
+        
+        sesion.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            guard let json = try? JSONDecoder().decode(Blob.self, from: data) else { return }
+            guard let dataDecode = Data(base64Encoded: json.content, options: .ignoreUnknownCharacters) else { return }
+            guard let image = UIImage(data: dataDecode) else { return }
+            completionHandler(image)
         }.resume()
     }
 }
