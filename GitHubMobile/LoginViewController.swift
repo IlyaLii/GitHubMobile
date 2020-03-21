d648009edf336f1e6bbfdaf0aa12e2a3e2e9a843
Ilya <agronat88@icloud.com>
2020-03-21 13:50:13 +0300

    keyboard bug fix in login page

--- GitHubMobile/LoginViewController.swift
+++ GitHubMobile/LoginViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     @IBOutlet weak var passwordTF: UITextField!
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     private var authService: AuthService!
-    
+    private var flagKeyboard = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard(sender:)))
+        passwordTF.delegate = self
+        emailTF.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(upUI), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(downUI), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardView(sender:)))
         view.addGestureRecognizer(tap)
         setupUI()
     }
@@ -30,9 +35,28 @@ class LoginViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    @objc func hideKeyboard(sender: UITapGestureRecognizer) {
-           self.view.endEditing(true)
-       }
+    @objc func hideKeyboardView(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    @objc func upUI(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame?.origin.y ?? 0
+            if self.loginButton.frame.origin.y > endFrameY {
+                flagKeyboard = true
+                self.topConstraint.constant -= 100
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @objc func downUI(notification: NSNotification) {
+        if flagKeyboard {
+            self.topConstraint.constant += 100
+            self.view.layoutIfNeeded()
+        }
+    }
     
     @IBAction func loginTapped(_ sender: UIButton) {
         if emailTF.text == "" || passwordTF.text == "" { return }
@@ -59,4 +83,12 @@ class LoginViewController: UIViewController {
             }
         }
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
 }
